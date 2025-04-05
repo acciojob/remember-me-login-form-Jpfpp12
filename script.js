4781200179c934b8afedf98019566144742df77b
Jpfpp12 <118419073+Jpfpp12@users.noreply.github.com>
2025-04-05 12:05:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let click = document.getElementById("submit")
+const checkbox = document.getElementById("checkbox")
+const username= document.getElementById("username")
+const password =document.getElementById("password")
+click.addEventListener("click",()=>{
+	alert("Logged in as " ,username)
+})
+if(checkbox.checked){
+	localStorage.setItem("username",username)
+}
